ce901d2940c30d1bebb809f27f7c474e6ffb2f32
royye6 <royye6@gmail.com>
2024-06-24 21:57:11 +0200

    feat: working user sign-in

--- image_library/views.py
+++ image_library/views.py
@@ -3,41 +3,42 @@ import requests, environ, os, json
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
-def index(request):
-    access_key = os.environ.get('ACCESS_KEY')
-
-    if not access_key:
-        raise ValueError('Access key not found in environment variables')
-    
-    if request.method == 'GET':
-        search_term = request.GET.get('search_term')
-        per_page = 1
-        url = f'https://api.unsplash.com/search/photos?client_id={access_key}&query={search_term}&per_page={per_page}&page=1'
-        response=requests.get(url)
-
-    if response.status_code == 200:
-        print(response.json())
-        return render(request, 'index.html', context={'unsplash_data': response.json()})
-    else:
-        return render(request, 'index.html', context={'error': 'API request failed'})
-
 
 # def index(request):
-#     return render(request, 'index.html')
+#     access_key = os.environ.get('ACCESS_KEY')
+
+#     if not access_key:
+#         raise ValueError('Access key not found in environment variables')
+    
+#     if request.method == 'GET':
+#         search_term = request.GET.get('search_term')
+#         per_page = 1
+#         url = f'https://api.unsplash.com/search/photos?client_id={access_key}&query={search_term}&per_page={per_page}&page=1'
+#         response=requests.get(url)
+
+#     if response.status_code == 200:
+#         print(response.json())
+#         return render(request, 'index.html', context={'unsplash_data': response.json()})
+#     else:
+#         return render(request, 'index.html', context={'error': 'API request failed'})
+
+
+def index(request):
+    return render(request, 'index.html')
 
 
 def register(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
-        if username != None and email != None and password != None and password2 != None:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if username != '' and email != '' and password != '' and password2 != '':
             if password == password2:
-                if User.objects.filter(email=email).exists:
+                if User.objects.filter(email=email).exists():
                     messages.info(request, 'Email already exists')
                     return redirect('register')
-                elif User.objects.filter(username=username):
+                elif User.objects.filter(username=username).exists():
                     messages.info(request, 'Username is taken')
                     return redirect('register')
                 else:
@@ -47,7 +48,7 @@ def register(request):
 
                     user_login = auth.authenticate(username=username, password=password)
                     auth.login(request, user_login)
-                    return redirect('profile.html')
+                    return redirect('profile')
             else:
                 messages.info(request, 'Passwords do not match')
                 return redirect('register')
@@ -60,8 +61,22 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        pass
-    return render(request, 'login.html')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username != '' and password != '':
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('profile')
+            else:
+                messages.info(request, 'Incorrect username or password')
+                return redirect('login') 
+        else:
+            messages.info(request, 'Please fill in all the required fields')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
 
 
 def profile(request):
